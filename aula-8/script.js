7020d137c52a03d2c7dfa4abf092ce17b84989e4
erlizaellen <erlizaellen@gmail.com>
2025-04-22 12:09:57 -0300

    calcular diaria

---
+++ aula-8/script.js
@@ -0,0 +1,64 @@
+//Armazenar os valores  dos inputs pelo id 
+const dia = document.getElementById("dia")
+const kms= document.getElementById("km")
+const categoria = document.getElementById("categoria")
+
+//Armazenar o valor do botao calcular pelo id
+const btn = document.getElementById("Calcular")
+
+
+//Armazenar o valor dos resultados dos id
+const valorDiarias = document.getElementById("result-diaria")
+const totalDias = document.getElementById("total-dias")
+const resultTotalDias = document.getElementById ("result-total-dias")
+const resultKm = document.getElementById("result-km")
+const totalKms = document.getElementById("total-kms")
+const resultTotalKms = document.getElementById("result-total-kms")
+const resultTotal =document.getElementById("result-total")
+
+function calcular(){
+    const economico = {
+        diaria : 100,
+        km: 0.20
+    }
+    const intermediario={
+        diaria:150,
+        km: 0.40
+    }
+    const luxo = {
+        diaria :250,
+        km: 0.80
+    }
+    let valorDiaria = 0;
+    let valorKm = 0; 
+
+    if (categoria.value == "economico"){
+        valorDiaria = economico.diaria
+        valorKm = economico.km
+    }
+
+    if (categoria.value == "intermediario"){
+        valorDiaria = intermediario.diaria
+        valorKm = intermediario.km
+    }
+    if (categoria.value == "luxo"){
+        valorDiaria = luxo.diaria
+        valorKm = luxo.km
+    }
+   const totalDiarias = valorDiaria * dia.value
+   const calcTotalKms = valorKm * kms.value
+   const total = totalDiarias + calcTotalKms
+
+   //exibir os resultados 
+   valorDiarias.innerHTML=`R$ ${valorDiaria.toFixed(2)}`
+   totalDias.innerHTML=`(${dia.value} Dias)`
+   resultTotalDias.innerHTML= `R$ ${totalDiarias.toFixed(2)}`
+   resultKm.innerHTML=`R$ ${valorKm.toFixed(2)}`
+   totalKms.innerHTML=`(${kms.value} Kms)`
+   resultTotalKms.innerHTML=`R$ ${calcTotalKms.toFixed(2)}`
+   resultTotal.innerHTML=`R$ ${total.toFixed(2)}`
+   
+
+}
+//Adicionar o evento do click no botao calcular 
+btn.addEventListener("click" , calcular)
